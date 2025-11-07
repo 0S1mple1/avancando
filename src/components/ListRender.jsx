8fca3891a98c11c5f7bf83e38500cd5b00b29cf9
0S1mple1 <gabriel.lucasmodesto@hotmail.com>
2025-11-07 18:53:20 -0300

    Add user list rendering to ListRender component

--- src/components/ListRender.jsx
+++ src/components/ListRender.jsx
@@ -2,12 +2,25 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(["João", "Maria", "José", "Ana"]);
+  const [users] = useState([
+    { id: 1, name: "João", age: 25 },
+    { id: 2, name: "Maria", age: 30 },
+    { id: 3, name: "José", age: 28 },
+    { id: 4, name: "Ana", age: 22 },
+  ]);
 
   return (
     <div>
       <ul>
         {list.map((item, i) => (
           <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
         ))}
       </ul>
     </div>

80fe82b37a2020ea739279209162895de46984b1
0S1mple1 <gabriel.lucasmodesto@hotmail.com>
2025-11-07 12:58:03 -0300

    Add App and ManageData components with initial implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
 
-import City from './assets/city.jpg';
+import City from "./assets/city.jpg";
+import ManageData from "./components/ManageData";
 function App() {
   return (
     <div className="App">
       <h1>Avancando em React</h1>
-      {/* Imagem tem public */ }
+      {/* Imagem tem public */}
       <div>
         <img src="/img1.jpg" alt="Paisagem" />
       </div>
@@ -13,6 +14,7 @@ function App() {
       <div>
         <img src={City} alt="Cidade" />
       </div>
+      <ManageData />
     </div>
   );
 }

9fffe27877dbe772c966367d76f9b52687c37682
0S1mple1 <gabriel.lucasmodesto@hotmail.com>
2025-11-07 11:34:44 -0300

    Refactor code structure for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import City from './assets/city.jpg';
 function App() {
   return (
     <div className="App">
       <h1>Avancando em React</h1>
+      {/* Imagem tem public */ }
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em src/assets */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </div>
   );
 }

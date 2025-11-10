ef487e21fef666963706751753a611c322975439
0S1mple1 <gabriel.lucasmodesto@hotmail.com>
2025-11-10 10:21:14 -0300

    Update ConditionalRender component to set initial state to false and display corresponding message

--- src/components/ConditionalRender.jsx
+++ src/components/ConditionalRender.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
 const ConditionalRender = () => {
-  const [x] = useState(true);
+  const [x] = useState(false);
 
   return (
     <div>
       <h1>Isso sera exibido</h1>
       {x && <p>Se x for true,sim!</p>}
+      {!x && <p>Se x for false,sim!</p>}
     </div>
   );
 };
